127d18c71d4b67f196160290c9d7942456987563
Pavel Dobiáš <git@paveldobias.eu>
2025-11-12 15:53:24 +0100

    date test

--- src/index.php
+++ src/index.php
@@ -1,4 +1,17 @@
 <?php
 //echo file_get_contents("test.html");
-echo date('d.m. Y.')
+function hezkyCesky($dayNumber) {
+    $vpohoArray = array("Pondeli","Utery","Streda","Ctvrtek","Patek","Sobota","Nedele");
+    return $vpohoArray[$dayNumber-1];
+}
+
+$date = date('d.m.Y.');
+$exploded = explode('.',$date);
+$day = $exploded[0];
+$month = $exploded[1];
+$year = $exploded[2];
+$timestamp = mktime(0,0,0,$month,$day,$year);
+echo $date + " je " + hezkyCesky(date('N'));
+echo $timestamp;
+
 ?>
