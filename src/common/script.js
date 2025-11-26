a6d5a769d00527e274f1e6ab00c98c7bf8c7cd51
Pavel Dobiáš <git@paveldobias.eu>
2025-11-26 10:19:25 +0100

    min size tweak

--- src/common/script.js
+++ src/common/script.js
@@ -15,7 +15,7 @@ main();
 
 function screenSizeAlert() {
 	const fontSize = parseInt(getComputedStyle(document.documentElement).fontSize.match(/\d+/));
-	const supportedSize = 18 * fontSize;
+	const supportedSize = 23 * fontSize;
 	if (window.innerWidth >= supportedSize) return; 
 	alert(`Your screen size is unsupported!\nYou might experience issues because the width of your window is less than ${Math.round(supportedSize)}px. Try lowering your default font size or resize the browser window to prevent this.\n\nScreen width = ${window.innerWidth}px\nFont size = ${fontSize}px`);
 }
