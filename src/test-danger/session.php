2bad1a3e676efcb110401a2e17531f66f547657a
Pavel Dobiáš <git@paveldobias.eu>
2025-11-26 15:38:37 +0100

    htmlsepecialchars

--- src/test-danger/session.php
+++ src/test-danger/session.php
@@ -16,6 +16,6 @@
     <title>Session</title>
 </head>
 <body>
-    <div>Tuto stránku jste již navštívili <?= isset($_COOKIE['counter']) ? $_COOKIE['counter'] : "0"; ?> krát.</div>
+    <div>Tuto stránku jste již navštívili <?= isset($_COOKIE['counter']) ? htmlspecialchars($_COOKIE['counter']) : "0"; ?> krát.</div>
 </body>
 </html>
