aa31551ccea8220b26ff8aa744873e09104bebe2
Pavel Dobiáš <git@paveldobias.eu>
2025-11-28 22:03:21 +0100

    404

---
+++ src/404/body.php
@@ -0,0 +1,6 @@
+<main>
+    <h1>404 Not Found!</h1>
+    <p>Looks like this URL doesn't exist on our website.</p>
+    <p>Path <?= $route; ?> can't be found.</p>
+    <img src="/~dobiapa2/assets/cracked-ice.webp" alt="Cracked Ice">
+</main>
