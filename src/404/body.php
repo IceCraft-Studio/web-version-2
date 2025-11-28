b3374e4923d78a4ee28672c226d7130a2e7bc86d
Pavel Dobiáš <git@paveldobias.eu>
2025-11-28 22:12:04 +0100

    404 styling

--- src/404/body.php
+++ src/404/body.php
@@ -3,7 +3,6 @@ $route = trim(parse_url(substr($_SERVER['REQUEST_URI'],11), PHP_URL_PATH),'/');
 ?>
 <main>
     <h1>404 Not Found!</h1>
-    <p>Looks like this URL doesn't exist on our website.</p>
-    <p>Path /<?= $route; ?> can't be found.</p>
+    <p>Looks like this URL path <span class="bold">/<?= $route; ?></span> doesn't exist on our website.</p>
     <img src="/~dobiapa2/assets/cracked-ice.webp" alt="Cracked Ice">
 </main>
