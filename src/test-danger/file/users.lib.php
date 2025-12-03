b8b3d5fa194024e1e3d16f9191567e7ef6a2b762
Pavel Dobiáš <git@paveldobias.eu>
2025-12-03 14:54:35 +0100

    cviceni 10

---
+++ src/test-danger/file/users.lib.php
@@ -0,0 +1,20 @@
+<?php
+
+function list_users() {
+    return [];
+}
+
+function get_user($id) {
+    return null;
+}
+
+function add_user($name, $email, $avatar) {
+}
+
+function delete_user($id) {
+}
+
+function edit_user($id, $name, $email, $avatar) {
+}
+
+?>
