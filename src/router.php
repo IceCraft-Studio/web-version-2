4edf88225994d3a3d421b3863813e040c86629f7
Pavel Dobiáš <git@paveldobias.eu>
2025-12-10 13:18:29 +0100

    test middleware

--- src/router.php
+++ src/router.php
@@ -1,32 +1,17 @@
 <?php
-require "api/libs/helpers.php";
+//# Libs
+require __DIR__ . "api/libs/helpers.php";
 
-$route = normalizeUriRoute($_SERVER['REQUEST_URI']);
-
-// Redirects
-if ($route == '') {
-    header("Location: ./home", true, 301);
-    exit;
-}
-
-// These files have to exist because they are included without checking
-$commonHead = __DIR__ . '/common/page/head.php';
-$commonBodyStart = __DIR__ . '/common/page/body-start.php';
-$commonBodyEnd = __DIR__ . '/common/page/body-end.php';
-// These are specific templates files for the route, can be .html or .php
-// If they are missing, 404 is used instead
-$routeHead  = __DIR__ . '/' . $route . '/head';
-$routeBody  = __DIR__ . '/' . $route . '/body';
-
+//# Functions
 /**
- * 
- * @param string $currentRoute
- * @return string 
+ * Primitive middleware that works by rerouting the request based on the route.
+ * @param string $currentRoute Current **normalized** route URI.
+ * @return string The new nornalized route URI, it returns the input one if no rerouting occurs.
  */
 function rerouteMiddleware($currentRoute) {
     $newRoute = $currentRoute;
     if (str_starts_with($currentRoute,"projects/")) {
-
+        $newRoute = "home";
     }
     return $newRoute;
 }
@@ -41,14 +26,33 @@ function includeTemplateFile($basePath) {
     include $file;
 }
 
+//# Script
+$route = normalizeUriRoute($_SERVER['REQUEST_URI']);
+
+// Redirects
+if ($route == '') {
+    header("Location: ./home", true, 301);
+    exit;
+}
+
+// These are files defining parts of the HTML common to all pages
+$commonHead = __DIR__ . '/common/page/head.php';
+$commonBodyStart = __DIR__ . '/common/page/body-start.php';
+$commonBodyEnd = __DIR__ . '/common/page/body-end.php';
+
+$route = rerouteMiddleware($route);
+
+// These are specific templates files for the route, can be .html or .php
+// If they are missing, 404 is used instead
+$routeHead  = __DIR__ . '/' . $route . '/head';
+$routeBody  = __DIR__ . '/' . $route . '/body';
+
 // GET pre-processor (optional)
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $routeGet  = __DIR__ . '/' . $route . '/get.php';
-    $getExists = file_exists($routeGet);
-    if ($getExists) {
-        include $routeGet;
-    }
+    include $routeGet;
 }
+
 // POST pre-processor (required)
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $routePost  = __DIR__ . '/' . $route . '/post.php';
@@ -56,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$postExists) {
         http_response_code(405);
         echo "POST is not allowed on this URL!";
-        exit();
+        exit; // `post.php` is required to allow POST on a URL, hence the `exit` function
     }
     include $routePost;
 }
