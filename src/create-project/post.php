b3c5f52afffd644e96ffdcbaedad7d3c1121ef5e
Pavel Dobiáš <git@paveldobias.eu>
2025-12-11 12:27:56 +0100

    no exit in post

--- src/create-project/post.php
+++ src/create-project/post.php
@@ -8,7 +8,9 @@ session_start();
 if (!isset($_POST['csrf-token']) || !isset($_SESSION['csrf-token']) || ($_POST['csrf-token'] !== $_SESSION['csrf-token'])) {
     $_SESSION['csrf-token'] = bin2hex(random_bytes(32));
     setcookie('csrf-violation','1',time()+60);
-    exit;
+    
+} else {
+    
 }
 setcookie('csrf-violation','0',time()+60);
 header('Location: /projects', true, 302);
