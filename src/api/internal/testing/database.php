321d1a096f534f6693b7eb7d195d7139ebdff79d
Pavel Dobiáš <git@paveldobias.eu>
2025-12-12 13:47:23 +0100

    more db test

--- src/api/internal/testing/database.php
+++ src/api/internal/testing/database.php
@@ -3,12 +3,28 @@ const DB_HOST = "localhost";
 const DB_NAME = "dobiapa2";
 const DB_USER = "dobiapa2";
 const DB_PASS = "webove aplikace";
-// pokusim se pripojit k DB stroji
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-if (!$link) {
-    echo "Nepodařilo se spojit s DB.<br>";
-    echo mysqli_connect_error();
-    exit;
-} else { 
-    echo "Asi se podařilo lol.";
+
+function connectToDatabase()
+{
+    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+    if (!$link) {
+        echo "Nepodařilo se spojit s DB.<br>";
+        echo mysqli_connect_error();
+        return;
+    } else {
+        echo "Asi se podařilo lol.";
+    }
+
+    $success = mysqli_select_db($link, DB_NAME);
+    if (!$success) {
+        echo "Nepodařilo se přepnout na správnou databázi";
+        return;
+    } else {
+        echo "Asi máme databázi";
+    }
+    return $link;
 }
+
+$db = connectToDatabase();
+$sqlQuery = "SHOW TABLES;";
+$result = mysqli_query($db,$sqlQuery);
