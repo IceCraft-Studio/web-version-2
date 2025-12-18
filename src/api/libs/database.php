b1ca322346babf4f128202dd413a3da8455b2661
Pavel Dobiáš <git@paveldobias.eu>
2025-12-18 10:08:50 +0100

    db uses env, move markdown test

--- src/api/libs/database.php
+++ src/api/libs/database.php
@@ -1,8 +1,8 @@
 <?php
-const DB_HOST = "localhost";
-const DB_NAME = "dobiapa2";
-const DB_USER = "dobiapa2";
-const DB_PASS = "webove aplikace";
+const DB_HOST = getenv("DB_HOSTNAME");
+const DB_NAME = getenv("DB_DATABASE");
+const DB_USER = getenv("DB_USERNAME");
+const DB_PASS = getenv("DB_PASSWORD");
 // pokusim se pripojit k DB stroji
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
 if (!$link) {
