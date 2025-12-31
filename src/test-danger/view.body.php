4d0e6e8082a10c729a351e545e6d1d2f11d68d7c
Pavel Dobiáš <git@paveldobias.eu>
2025-12-31 20:37:17 +0100

    more testing

--- src/test-danger/view.body.php
+++ src/test-danger/view.body.php
@@ -1,5 +1,5 @@
 <?php
 $dbConnect = DbConnect::getConnection(getDbAccessObject());
-var_dump(dbQuery($dbConnect,'SHOW TABLES'));
+var_dump(dbQuery($dbConnect,'SELECT * FROM `role` WHERE `id` = ?',"s",["admin"]));
 phpinfo();
 ?>
