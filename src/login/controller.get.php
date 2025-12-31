1ab9a2294123a90b7a3a456e207eba233fd5ee4b
Pavel Dobiáš <git@paveldobias.eu>
2025-12-31 23:19:24 +0100

    redirect login and register when logged in

--- src/login/controller.get.php
+++ src/login/controller.get.php
@@ -1,4 +1,8 @@
 <?php
+if (verifySession($_COOKIE['token'] ?? '') != null) {
+    header('Location: /~dobiapa2/profile',true,302);
+    exit;
+}
 initCsrf();
 
 $viewState = ViewData::getInstance();
--- src/register/controller.get.php
+++ src/register/controller.get.php
@@ -1,2 +1,6 @@
 <?php
+if (verifySession($_COOKIE['token'] ?? '') != null) {
+    header('Location: /~dobiapa2/profile',true,302);
+    exit;
+}
 initCsrf();
