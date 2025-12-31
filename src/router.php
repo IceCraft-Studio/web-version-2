8dd9be4b72db7368282a1c83c0ebf2186a0812ec
Pavel Dobiáš <git@paveldobias.eu>
2025-12-31 18:39:45 +0100

    load db connect in router

--- src/router.php
+++ src/router.php
@@ -3,6 +3,7 @@
 require __DIR__ . "/api/libs/secure/database-env.php";
 require __DIR__ . "/api/libs/helpers.php";
 require __DIR__ . "/api/libs/classes/view-data.php";
+require __DIR__ . "/api/libs/classes/db-connect.php";
 
 //# Constants
 /**
