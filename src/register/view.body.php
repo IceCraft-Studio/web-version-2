610f1f99467d1892fa4feaaec9afc49f5e57e594
Pavel Dobiáš <git@paveldobias.eu>
2026-01-01 21:16:03 +0100

    styless

--- src/register/view.body.php
+++ src/register/view.body.php
@@ -4,7 +4,7 @@ $prefillUsername = htmlspecialchars($viewState->get('form-username'));
 $showError = true;
 $errorMessage = '';
 
-switch ($viewState->get('register-error',RegisterFormError::None)) {
+switch ($viewState->get('register-error', RegisterFormError::None)) {
     case RegisterFormError::None:
         $showError = false;
         break;
@@ -34,27 +34,33 @@ $csrfToken = $_SESSION['csrf-token'];
     <form method="POST" action="">
         <div class="row">
             <label for="username">Username:</label>
-            <input type="text" maxlength="32" name="username" value="<?= $prefillUsername ?>" required>
-            <div class="indicator" data-for="username"></div>
+            <div class="input-group">
+                <input type="text" maxlength="32" name="username" value="<?= $prefillUsername ?>" required>
+                <div class="indicator" data-for="username"></div>
+            </div>
         </div>
         <div class="row">
             <label for="password">Password:</label>
-            <input type="password" maxlength="128" name="password" required>
-            <div class="indicator" data-for="password"></div>
+            <div class="input-group">
+                <input type="password" maxlength="128" name="password" required>
+                <div class="indicator" data-for="password"></div>
+            </div>
         </div>
         <div class="row">
             <label for="confirm-password">Confirm Password:</label>
-            <input type="password" maxlength="128" name="confirm-password" required>
-            <div class="indicator" data-for="confirm-password"></div>
+            <div class="input-group">
+                <input type="password" maxlength="128" name="confirm-password" required>
+                <div class="indicator" data-for="confirm-password"></div>
+            </div>
         </div>
         <input type="hidden" name="csrf-token" value="<?= $csrfToken ?>">
         <div class="row">
             <input type="submit" value="Register" disabled>
         </div>
         <?php if ($showError): ?>
-        <div class="row bold color-required">
-            <?= $errorMessage ?>
-        </div>
+            <div class="row bold color-required">
+                <?= $errorMessage ?>
+            </div>
         <?php endif; ?>
     </form>
     <div>
