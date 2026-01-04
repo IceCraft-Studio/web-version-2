4330c6b45489a8f7f3ec752e1ef8d7fb1ced388c
Pavel Dobiáš <git@paveldobias.eu>
2026-01-04 15:38:36 +0100

    smol changes

--- src/profile/controller.post.php
+++ src/profile/controller.post.php
@@ -6,6 +6,7 @@ const MAX_ALLOWED_IMAGE_SIZE_MB = 8;
 
 $viewState = ViewData::getInstance();
 
+//## Authentication
 $csrfLegit = validateCsrf();
 
 if (!$csrfLegit) {
@@ -19,6 +20,7 @@ if ($username === '') {
     redirect('/~dobiapa2/login');
 }
 
+//## Change Profile Picture
 if (isset($_POST['delete-profile-picture'])) {
     if(saveUserProfilePicture($username,'')) {
         $viewState->set('picture-update-state',PictureUpdateState::Success);        
@@ -44,6 +46,7 @@ if (isset($_POST['delete-profile-picture'])) {
     $viewState->set('picture-update-state',PictureUpdateState::NoUpdate);        
 }
 
+//## Profile Change
 $displayName = trim($_POST['display-name'] ?? '');
 $email = trim($_POST['email'] ?? '');
 $socialWebsite = trim($_POST['social-website'] ?? '');
@@ -51,11 +54,8 @@ $socialReddit = trim($_POST['social-reddit'] ?? '');
 $socialTwitter = trim($_POST['social-twitter'] ?? '');
 $socialInstagram = trim($_POST['social-instagram'] ?? '');
 $socialDiscord = trim($_POST['social-discord'] ?? '');
-$password = $_POST['password'] ?? '';
-$passwordNew = $_POST['password-new'] ?? '';
-$passwordConfirm = $_POST['password-confirm'] ?? '';
 
-// Profile Change
+
 $profileUpdated = false;
 $profileUpdateSuccess = true;
 $originalUserData = getUserData($username);
@@ -77,7 +77,6 @@ if ($originalUserData['display_name'] != $displayName) {
 }
 if ($originalUserData['email'] != $email) {
     if (!validateUserEmail($email)) {
-        $viewState->set('profile-update-state',ProfileUpdateState::Failure);
         $viewState->set('profile-update-fail-message','Profile update failed! The email must be at most 200 characters long and follow the format "name@domain.tld".');
         $changeSuccess = false;
     } else {
@@ -87,31 +86,57 @@ if ($originalUserData['email'] != $email) {
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 if ($originalUserData['social_website'] != $socialWebsite) {
-    $changeSuccess = changeUserSocial($username,UserSocial::Website,$socialWebsite);
+    if (!validateUserSocial($socialWebsite)) {
+        $viewState->set('profile-update-fail-message','Profile update failed! The social link can\'t be longer than 150 characters.');
+        $changeSuccess = false;
+    } else {
+        $changeSuccess = changeUserSocial($username,UserSocial::Website,$socialWebsite);
+    }
     $profileUpdated = true;
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 if ($originalUserData['social_reddit'] != $socialReddit) {
-    $changeSuccess = changeUserSocial($username,UserSocial::Reddit,$socialReddit);
+    if (!validateUserSocial($socialReddit)) {
+        $viewState->set('profile-update-fail-message','Profile update failed! The social link can\'t be longer than 150 characters.');
+        $changeSuccess = false;
+    } else {
+        $changeSuccess = changeUserSocial($username,UserSocial::Reddit,$socialReddit);
+    }
     $profileUpdated = true;
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 if ($originalUserData['social_twitter'] != $socialTwitter) {
-    $changeSuccess = changeUserSocial($username,UserSocial::Twitter,$socialTwitter);
+    if (!validateUserSocial($socialTwitter)) {
+        $viewState->set('profile-update-fail-message','Profile update failed! The social link can\'t be longer than 150 characters.');
+        $changeSuccess = false;
+    } else {
+        $changeSuccess = changeUserSocial($username,UserSocial::Twitter,$socialTwitter);
+    }
     $profileUpdated = true;
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 if ($originalUserData['social_instagram'] != $socialInstagram) {
-    $changeSuccess = changeUserSocial($username,UserSocial::Instagram,$socialInstagram);
+    if (!validateUserSocial($socialInstagram)) {
+        $viewState->set('profile-update-fail-message','Profile update failed! The social link can\'t be longer than 150 characters.');
+        $changeSuccess = false;
+    } else {
+        $changeSuccess = changeUserSocial($username,UserSocial::Instagram,$socialInstagram);
+    }
     $profileUpdated = true;
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 if ($originalUserData['social_discord'] != $socialDiscord) {
-    $changeSuccess = changeUserSocial($username,UserSocial::Discord,$socialDiscord);
+    if (!validateUserSocial($socialDiscord)) {
+        $viewState->set('profile-update-fail-message','Profile update failed! The social link can\'t be longer than 150 characters.');
+        $changeSuccess = false;
+    } else {
+        $changeSuccess = changeUserSocial($username,UserSocial::Discord,$socialDiscord);
+    }
     $profileUpdated = true;
     $profileUpdateSuccess = $profileUpdateSuccess ? $changeSuccess : false;
 }
 
+// ## Profile Update State
 if ($profileUpdated) {
     if ($profileUpdateSuccess) {
         $viewState->set('profile-update-state',ProfileUpdateState::Success);
@@ -122,7 +147,11 @@ if ($profileUpdated) {
     $viewState->set('profile-update-state',ProfileUpdateState::NoUpdate);
 }
 
-// Password Update
+//## Password Update
+$password = $_POST['password'] ?? '';
+$passwordNew = $_POST['password-new'] ?? '';
+$passwordConfirm = $_POST['password-confirm'] ?? '';
+
 $viewState->set('password-update-state',PasswordUpdateState::NoUpdate);
 if ($passwordNew != '') {
     if (!validatePassword($passwordNew)) {
@@ -146,5 +175,5 @@ if ($passwordNew != '') {
     $viewState->set('password-update-state',PasswordUpdateState::Success);
 }
 
-
+//## Prepare Display Information
 include __DIR__ . '/controller.get.php';
--- src/upload-project/controller.post.php
+++ src/upload-project/controller.post.php
@@ -1,10 +1,39 @@
 <?php
+require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/api/libs/models/project.php';
 // Validation
 $csrfLegit = validateCsrf();
 if (!$csrfLegit) {
     
 }
+$viewState = ViewData::getInstance();
 
+$username = $viewState->get('verified-username','');
+
+if ($username === '') {
+    http_response_code(401);
+    return;
+}
+
+$projectSlug = $_POST['slug'] ?? '';
+$projectCategory = $_POST['category'] ?? '';
+$projectEditing = $_POST['editing'] ?? '0';
+$checkExistingProject = getProjectData($projectCategory,$projectSlug);
+if ($projectEditing === '1') {
+    if ($checkExistingProject === false) {
+        http_response_code(400);
+        return;
+    }
+    // failed validating gives control to get controller and sets the edit parameters in viewData
+
+} else if ($projectEditing === '0') {
+    if ($checkExistingProject !== false) {
+        //handle slug taken
+    }
+    //failed validating gives control to get controller
+} else {
+    http_response_code(400);
+    return;
+}
 
 
 // Processing
