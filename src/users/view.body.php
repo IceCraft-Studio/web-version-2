562cdd5117cf175fd50710a005fd85c1b6a131d2
Pavel Dobiáš <git@paveldobias.eu>
2026-01-06 12:24:47 +0100

    lets see

--- src/users/view.body.php
+++ src/users/view.body.php
@@ -3,23 +3,31 @@
 $viewState = ViewData::getInstance();
 $usersList = $viewState->get('users-list');
 
+
 function generateUserTableData($usersList) {
     foreach ($usersList as $user) {
-        $user['username']
-        echo '<td><img src="' . $user['username']  . '" alt="' . $user['username'] . '"></td>';
-        echo '<td>' .  . '</td>'
-        echo '<td>' .  . '</td>'
-        echo '<td>' .  . '</td>'
+        $userUsername = $user['username'] ?? '';
+        $dateCreated = date('d/m/Y G:i:s',strtotime($user['datetime_created']));
+        echo '<tr>';
+        echo '<td><img src="' . getUserPictureLink($userUsername)  . '" alt="' . $userUsername . '\'s Profile Picture"></td>';
+        echo '<td><a href="' . getUserLink($userUsername) . '' . '</td>';
+        echo '<td>' . $user['display_name'] ?? '' . '</td>';
+        echo '<td>' . $user['role'] ?? '' . '</td>';
+        echo '<td>' . $dateCreated . '</td>';
+        echo '</tr>';
     }
 }
 
 ?>
 <main>
+    <h1>Manage Users</h1>
+    <p>Here you are provided with a list of all users, openning the link on their username leads to their user page where you can manage their accounts.</p>
     <table>
         <tr>
             <th>Picture</th>
             <th>Username</th>
             <th>Display Name</th>
+            <th>Role</th>
             <th>Datetime Created</th>
         </tr>
         <?= generateUserTableData($usersList) ?>
