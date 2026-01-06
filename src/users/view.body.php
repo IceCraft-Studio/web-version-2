df7e064118cb3dd46306aa83aff140b41de3669f
Pavel Dobiáš <git@paveldobias.eu>
2026-01-06 13:17:57 +0100

    oki

--- src/users/view.body.php
+++ src/users/view.body.php
@@ -4,12 +4,13 @@ $viewState = ViewData::getInstance();
 $usersList = $viewState->get('users-list');
 
 
-function generateUserTableData($usersList) {
+function generateUserTableData($usersList)
+{
     foreach ($usersList as $user) {
         $userUsername = $user['username'] ?? '';
-        $dateCreated = date('d/m/Y G:i:s',strtotime($user['datetime_created']));
+        $dateCreated = date('d/m/Y G:i:s', strtotime($user['datetime_created']));
         echo '<tr>';
-        echo '<td><img src="' . getUserPictureLink($userUsername)  . '" alt="' . $userUsername . '\'s Profile Picture"></td>';
+        echo '<td><img src="' . getUserPictureLink($userUsername) . '" alt="' . $userUsername . '\'s Profile Picture"></td>';
         echo '<td><a href="' . getUserLink($userUsername) . '">' . $userUsername . '</a></td>';
         echo '<td>' . $user['display_name'] ?? '' . '</td>';
         echo '<td>' . $user['role'] ?? '' . '</td>';
@@ -27,57 +28,67 @@ $currentOrder = $_GET['order'] ?? 'asc';
 ?>
 <main>
     <h1 id="page-top">Manage Users</h1>
-    <p>Here you are provided with a list of all users, openning the link on their username leads to their user page where you can manage their accounts.</p>
+    <p>Here you are provided with a list of all users, openning the link on their username leads to their user page
+        where you can manage their accounts.</p>
     <div id="page-controls">
         <div class="page-links">
 
         </div>
         <div class="page-form">
             <form method="get">
-                <label for="select-page-size">Users per page:</label>
-                <select id="select-page-size" name="size">
-                    <option value="10" <?= $currentSize === '10' ? 'selected' : '' ?>>10</option>
-                    <option value="25" <?= $currentSize === '25' ? 'selected' : '' ?>>25</option>
-                    <option value="50" <?= $currentSize === '50' ? 'selected' : '' ?>>50</option>
-                    <option value="100" <?= $currentSize === '100' ? 'selected' : '' ?>>100</option>
-                    <option value="200" <?= $currentSize === '200' ? 'selected' : '' ?>>200</option>
-                </select>
-                <label for="select-page-sort">Filter Role:</label>
-                <select id="select-page-sort" name="role">
-                    <option value="" <?= $currentRole === '' ? 'selected' : '' ?>>All Roles</option>
-                    <option value="ban" <?= $currentRole === 'ban' ? 'selected' : '' ?>>Banned</option>
-                    <option value="user" <?= $currentRole === 'user' ? 'selected' : '' ?>>User</option>
-                    <option value="admin" <?= $currentRole === 'admin' ? 'selected' : '' ?>>Administrator</option>
-                    <option value="owner" <?= $currentRole === 'owner' ? 'selected' : '' ?>>Website Owner</option>
-                </select>
-                <label for="select-page-sort">Sort by:</label>
-                <select id="select-page-sort" name="sort">
-                    <option value="username" <?= $currentSort === 'username' ? 'selected' : '' ?>>Username</option>
-                    <option value="display_name" <?= $currentSort === 'display_name' ? 'selected' : '' ?>>Display Name</option>
-                    <option value="modified" <?= $currentSort === 'modified' ? 'selected' : '' ?>>Modified</option>
-                </select>
-                <label for="select-page-order">Sort order:</label>
-                <select id="select-page-order" name="order" >
-                    <option value="asc" <?= $currentOrder === 'asc' ? 'selected' : '' ?>>Ascending</option>
-                    <option value="desc" <?= $currentOrder === 'desc' ? 'selected' : '' ?>>Descending</option>
-                </select>
+                <div>
+                    <label for="select-page-size">Users per page:</label>
+                    <select id="select-page-size" name="size">
+                        <option value="10" <?= $currentSize === '10' ? 'selected' : '' ?>>10</option>
+                        <option value="25" <?= $currentSize === '25' ? 'selected' : '' ?>>25</option>
+                        <option value="50" <?= $currentSize === '50' ? 'selected' : '' ?>>50</option>
+                        <option value="100" <?= $currentSize === '100' ? 'selected' : '' ?>>100</option>
+                        <option value="200" <?= $currentSize === '200' ? 'selected' : '' ?>>200</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="select-page-sort">Filter Role:</label>
+                    <select id="select-page-sort" name="role">
+                        <option value="" <?= $currentRole === '' ? 'selected' : '' ?>>All Roles</option>
+                        <option value="ban" <?= $currentRole === 'ban' ? 'selected' : '' ?>>Banned</option>
+                        <option value="user" <?= $currentRole === 'user' ? 'selected' : '' ?>>User</option>
+                        <option value="admin" <?= $currentRole === 'admin' ? 'selected' : '' ?>>Administrator</option>
+                        <option value="owner" <?= $currentRole === 'owner' ? 'selected' : '' ?>>Website Owner</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="select-page-sort">Sort by:</label>
+                    <select id="select-page-sort" name="sort">
+                        <option value="username" <?= $currentSort === 'username' ? 'selected' : '' ?>>Username</option>
+                        <option value="display_name" <?= $currentSort === 'display_name' ? 'selected' : '' ?>>Display Name
+                        </option>
+                        <option value="modified" <?= $currentSort === 'modified' ? 'selected' : '' ?>>Modified</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="select-page-order">Sort order:</label>
+                    <select id="select-page-order" name="order">
+                        <option value="asc" <?= $currentOrder === 'asc' ? 'selected' : '' ?>>Ascending</option>
+                        <option value="desc" <?= $currentOrder === 'desc' ? 'selected' : '' ?>>Descending</option>
+                    </select>
+                </div>
                 <input type="submit" value="Apply">
             </form>
         </div>
     </div>
     <div class="horizontal-scroll">
-    <table>
-        <tr>
-            <th>Picture</th>
-            <th>Username</th>
-            <th>Display Name</th>
-            <th>Role</th>
-            <th>Datetime Created</th>
-        </tr>
-        <?php
-         generateUserTableData($usersList) 
-        ?>
-    </table>
+        <table>
+            <tr>
+                <th>Picture</th>
+                <th>Username</th>
+                <th>Display Name</th>
+                <th>Role</th>
+                <th>Datetime Created</th>
+            </tr>
+            <?php
+            generateUserTableData($usersList)
+                ?>
+        </table>
     </div>
     <a href="#page-top">Return to top.</a>
 </main>
