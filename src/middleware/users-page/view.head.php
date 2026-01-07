fc62c1a8ec1138417a1d4ed6201adb0ecc426aab
Pavel Dobiáš <git@paveldobias.eu>
2026-01-07 20:02:17 +0100

    sss

--- src/middleware/users-page/view.head.php
+++ src/middleware/users-page/view.head.php
@@ -1,5 +1,5 @@
 <?php
 $viewState = ViewData::getInstance();
 ?>
-<title><?= $viewState->get('page-display-name') ?> - User | IceCraft Studio</title>
+<title><?= $viewState->get('page-display-name','') ?> - User | IceCraft Studio</title>
 <link href="/~dobiapa2/middleware/users-page/style.css" rel="stylesheet" type="text/css">
