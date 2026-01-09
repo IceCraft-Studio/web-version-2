c9db26c59282c3421725f74a8e9ffcfd7cd9c617
Pavel Dobiáš <git@paveldobias.eu>
2026-01-09 19:47:57 +0100

    username

--- src/middleware/projects/view.body.php
+++ src/middleware/projects/view.body.php
@@ -3,6 +3,8 @@ require $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/api/libs/paging.php';
 
 $viewState = ViewData::getInstance();
 
+$viewerUsername = $viewState->get('verified-username','');
+
 // Project Data
 $projectsList = $viewState->get('projects-list',[]);
 $projectsCategory = $viewState->get('projects-category','');
