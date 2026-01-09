8160993c88ea34ed8613140614c9171b4674dcef
Pavel Dobiáš <git@paveldobias.eu>
2026-01-09 20:39:44 +0100

    small change

--- src/about/view.body.php
+++ src/about/view.body.php
@@ -3,14 +3,14 @@
     <div>
         <h2>What is this website</h2>
         <p>
-            We started in 2020 as a small Minecraft community collaborating on Bedrock maps and addons with other 
-            content creators. Throughout the years we've made a bunch of smaller projects and now we are happy to 
+            We started in 2020 as a small Minecraft community collaborating on Bedrock maps and addons with other
+            content creators. Throughout the years we've made a bunch of smaller projects and now we are happy to
             provide you a website where you can find all of our projects and also register to share your own.
 
             This website gets you simple by default ad-free exprience to find projects of all different kinds
             and anyone is welcome to upload their own to expand our ever growing collection.
 
-            The main features of the website include creating a simple user profile for your presence on the website 
+            The main features of the website include creating a simple user profile for your presence on the website
             and once you have your profile you can publish your own projects into any of the categories
             we allow to be uploaded.
 
@@ -22,17 +22,19 @@
         <p>
             We also have accounts across multiple other sites where you can find us too!
         </p>
-        <div>
-            <a href="github.com/IceCraft-Studio">GitHub Page</a>
-        </div>
-        <div>
-            <a href="https://mcpedl.com/user/paveldobcz23/">MCPEDL Page</a>
-        </div>
-        <div>
-            <a href="https://modrinth.com/organization/icecraft-studio">Modrinth</a>
-        </div>
-        <div>
-            <a href="https://marketplace.visualstudio.com/publishers/IceCraftStudio">VS Code Marketplace</a>
-        </div>
+        <ul>
+            <li>
+                <a href="github.com/IceCraft-Studio">GitHub Page</a>
+            </li>
+            <li>
+                <a href="https://mcpedl.com/user/paveldobcz23/">MCPEDL Page</a>
+            </li>
+            <li>
+                <a href="https://modrinth.com/organization/icecraft-studio">Modrinth</a>
+            </li>
+            <li>
+                <a href="https://marketplace.visualstudio.com/publishers/IceCraftStudio">VS Code Marketplace</a>
+            </li>
+        </ul>
     </div>
 </main>
