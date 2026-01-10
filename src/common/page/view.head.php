f757920dbab86eb7cc8f00f1d84ceb36d220e0c9
Pavel Dobiáš <git@paveldobias.eu>
2026-01-10 19:09:45 +0100

    meta charset

--- src/common/page/view.head.php
+++ src/common/page/view.head.php
@@ -1,4 +1,4 @@
-<meta charset="UTF-8">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="/~dobiapa2/assets/icecraft-logo.svg" rel="icon">
 <link href="/~dobiapa2/common/style.css" rel="stylesheet" type="text/css">
