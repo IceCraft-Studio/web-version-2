759c8b0e13961d6a37f5bd8792f32626aa3bbc96
Pavel Dobiáš <git@paveldobias.eu>
2026-01-12 10:23:23 +0100

    small upload project style tweak

--- src/upload-project/view.body.php
+++ src/upload-project/view.body.php
@@ -362,10 +362,10 @@ $csrfToken = getCsrf('upload-project');
                 </ul>
             </div>
         </div>
-        <h2>Downloads & Links</h2>
+        <h2 id="downloads-links-heading">Downloads & Links</h2>
         <div class="field">
             <div>
-                The project needs to provide at least 1 file or link and at most 5 of each for the audience to download.
+                The project needs to provide <span class="bold color-required">at least 1 file or link</span> and at most 5 of each for the audience to download.
             </div>
             <div>
                 Max file upload size is 30MB. File names are preserved but modified to be safe. Make sure the link is a unique valid HTTP(S) URL no longer than 200 charactetrs.
